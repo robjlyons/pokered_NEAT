10708edda6841bcd72061af22b40b777de352900
robjlyons <81177492+robjlyons@users.noreply.github.com>
2024-06-10 13:12:34 +0100

    Update home.asm

--- home.asm
+++ home.asm
@@ -80,10 +80,3 @@ INCLUDE "home/random.asm"
 INCLUDE "home/predef.asm"
 INCLUDE "home/hidden_objects.asm"
 INCLUDE "home/predef_text.asm"
-
-SECTION "Home Initialization", ROMX
-
-GameStartInit:
-    call InitPopulation  ; Initialize NEAT population
-    ; Other initialization code...
-    ret
