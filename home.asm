1df40c0e75e9f75e213f8e34eee86e79b1c194a3
robjlyons <81177492+robjlyons@users.noreply.github.com>
2024-06-10 11:50:19 +0100

    Update home.asm

--- home.asm
+++ home.asm
@@ -83,9 +83,25 @@ INCLUDE "home/predef.asm"
 INCLUDE "home/hidden_objects.asm"
 INCLUDE "home/predef_text.asm"
 
-SECTION "Home Initialization", ROM0
+SECTION "NEAT Data Definitions", WRAM0
 
-GameStartInit:
-    call InitPopulation  ; Initialize NEAT population
-    ; Other initialization code...
-    ret
+; NEAT related data
+wNEAT_EnemyHP:        ds 1
+wNEAT_PlayerHP:       ds 1
+wNEAT_EnemyMoves:     ds 4
+wNEAT_EnemyStatus:    ds 1
+wNEAT_PlayerStatus:   ds 1
+wNEAT_SelectedMove:   ds 1
+wNEAT_BattleMonStatus: ds 1
+wNEAT_EnemyMonStatus: ds 1
+wNEAT_wBattleMonHP:   ds 1
+
+; Define memory locations for random byte generation
+SomeMemoryLocation:   ds 1
+AnotherMemoryLocation: ds 1
+
+; Define battle state symbols
+wPlayerMonHP:         ds 1
+wEnemyMonHP:          ds 1
+wPlayerStatus:        ds 1
+wEnemyStatus:         ds 1
