428f593e683939fcac00b75d0dd0b92862f62875
robjlyons <81177492+robjlyons@users.noreply.github.com>
2024-06-17 11:49:48 +0100

    Update variables.asm

--- data/variables.asm
+++ data/variables.asm
@@ -2,14 +2,12 @@ SECTION "NEAT Variables", WRAM0
 
 wNEAT_EnemyHP:        ds 1
 wNEAT_PlayerHP:       ds 1
-wNEAT_EnemyMoves:     ds 4
 wNEAT_EnemyStatus:    ds 1
 wNEAT_PlayerStatus:   ds 1
 wNEAT_SelectedMove:   ds 1
 wNEAT_BattleMonStatus: ds 1
 wNEAT_EnemyMonStatus: ds 1
-wNEAT_BattleMonHP:    ds 1
-wValidatedMove:       ds 1
+wNEAT_BattleMonHP:    ds 1  ; Fixed variable name
 
 ; Define memory locations for random byte generation
 SomeMemoryLocation:   ds 1
