b5ae033a3d5f9a5bd6e4c4e586e8c0a0bf2a433a
robjlyons <81177492+robjlyons@users.noreply.github.com>
2024-06-18 11:18:17 +0100

    Update variables.asm

--- data/variables.asm
+++ data/variables.asm
@@ -2,16 +2,16 @@ SECTION "NEAT Variables", WRAM0
 
 wNEAT_EnemyHP:        ds 1
 wNEAT_PlayerHP:       ds 1
+wNEAT_EnemyStatus:    ds 1
+wNEAT_PlayerStatus:   ds 1
 wNEAT_SelectedMove:   ds 1
-
-; Reusing existing battle state symbols
-wBattleMonHP:         ds 1
-wEnemyMonHP:          ds 1
-wBattleMonStatus:     ds 1
-wEnemyMonStatus:      ds 1
-
-wValidatedMove:       ds 1  ; Added wValidatedMove
 
 ; Define memory locations for random byte generation
 SomeMemoryLocation:   ds 1
 AnotherMemoryLocation: ds 1
+
+; Define battle state symbols
+wPlayerMonHP:         ds 1
+wEnemyMonHP:          ds 1
+wPlayerStatus:        ds 1
+wEnemyStatus:         ds 1
